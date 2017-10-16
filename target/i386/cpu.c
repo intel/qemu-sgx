b43220697f76555eb5a834528ce9a90e9438d57d
Sean Christopherson <sean.j.christopherson@intel.com>
2017-10-16 07:55:57 -0700

    target-i386: SGX: add SGX CPUID and MSR defines
    Add CPUID defines for SGX and SGX Launch Control (LC), as well as
    defines for their associated FEATURE_CONTROL MSR bits.  Define the
    Launch Enclave Public Key Hash MSRs (LE Hash MSRs), which exist
    when SGX LC is present (in CPUID), and are writable when SGX LC is
    enabled (in FEATURE_CONTROL).
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -961,7 +961,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
     },
     [FEAT_7_0_EBX] = {
         .feat_names = {
-            "fsgsbase", "tsc-adjust", NULL, "bmi1",
+            "fsgsbase", "tsc-adjust", "sgx", "bmi1",
             "hle", "avx2", NULL, "smep",
             "bmi2", "erms", "invpcid", "rtm",
             NULL, NULL, "mpx", NULL,
@@ -984,7 +984,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             "la57", NULL, NULL, NULL,
             NULL, NULL, "rdpid", NULL,
             NULL, "cldemote", NULL, NULL,
-            NULL, NULL, NULL, NULL,
+            NULL, NULL, "sgxlc", NULL,
         },
         .cpuid_eax = 7,
         .cpuid_needs_ecx = true, .cpuid_ecx = 0,
