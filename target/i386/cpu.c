32af1c2c318478b4ec1679ae7c58fac5812de194
Sean Christopherson <sean.j.christopherson@intel.com>
2017-10-16 08:58:57 -0700

    target-i386: SGX: update CPUID bits based on user input and hardware support
    SGX cannot be emulated in software.  If hardware does not support
    enabling SGX and/or SGX Launch Control, then update the VM's CPUID
    values accordingly.  The user can further restrict SGX via feature
    bits.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -3350,6 +3350,25 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
                 *ecx |= CPUID_7_0_ECX_OSPKE;
             }
             *edx = env->features[FEAT_7_0_EDX]; /* Feature flags */
+
+            /*
+             * SGX cannot be emulated in software.  If hardware does not
+             * support enabling SGX and/or SGX flexible launch control,
+             * then we need to update the VM's CPUID values accordingly.
+             */
+            if ((*ebx & CPUID_7_0_EBX_SGX) &&
+                (!kvm_enabled() ||
+                 !(kvm_arch_get_supported_cpuid(cs->kvm_state, 0x7, 0, R_EBX) &
+                    CPUID_7_0_EBX_SGX))) {
+                *ebx &= ~CPUID_7_0_EBX_SGX;
+            }
+
+            if ((*ecx & CPUID_7_0_ECX_SGX_LC) &&
+                (!(*ebx & CPUID_7_0_EBX_SGX) || !kvm_enabled() ||
+                 !(kvm_arch_get_supported_cpuid(cs->kvm_state, 0x7, 0, R_ECX) &
+                    CPUID_7_0_ECX_SGX_LC))) {
+                *ecx &= ~CPUID_7_0_ECX_SGX_LC;
+            }
         } else {
             *eax = 0;
             *ebx = 0;
@@ -3436,6 +3455,38 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
         }
         break;
     }
+    case 0x12:
+        if (!kvm_enabled() ||
+           !(env->features[FEAT_7_0_EBX] & CPUID_7_0_EBX_SGX)) {
+            *eax = *ebx = *ecx = *edx = 0;
+            break;
+        }
+
+        /*
+         * The SGX sub-leafs, CPUID.0x12.{0x0, 0x1, 0x2, ...} are heavily
+         * dependent on hardware and system (KVM) support.  For most entries
+         * we rely entirely on KVM to manage the CPUID values as there is
+         * (currently) no value in deviating from hardware, and if there
+         * were some perceived value, e.g. restricting SECS.ATTRIBUTES.XFRM,
+         * it would require KVM support that does not exist, e.g. trapping
+         * ECREATE to enforce the user-specified restrictions.  The exception
+         * is the SGX features word (see below).
+         */
+        *eax = kvm_arch_get_supported_cpuid(kvm_state, 0x12, count, R_EAX);
+        *ebx = kvm_arch_get_supported_cpuid(kvm_state, 0x12, count, R_EBX);
+        *ecx = kvm_arch_get_supported_cpuid(kvm_state, 0x12, count, R_ECX);
+        *edx = kvm_arch_get_supported_cpuid(kvm_state, 0x12, count, R_EDX);
+
+        /*
+         * CPUID.0x12.0x0.EAX enumerates SGX features, e.g. SGX1 and SGX2
+         * instruction sets, and is exposed to the user.  The user can't
+         * override features not supported by hardware/KVM, but they can
+         * further restrict which features are exposed to the guest.
+         */
+        if (count == 0) {
+            *eax &= env->features[FEAT_12_0_EAX];
+        }
+        break;
     case 0x40000000:
         /*
          * CPUID code in kvm_arch_init_vcpu() ignores stuff
